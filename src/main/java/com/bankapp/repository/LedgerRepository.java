31f05d90608e967ef5212b34c50404bce13d6ecc
Victor1302 <victoor2k19@gmail.com>
2026-01-20 20:42:18 -0300

    adding ledgerRepository

---
+++ src/main/java/com/bankapp/repository/LedgerRepository.java
@@ -0,0 +1,11 @@
+package com.bankapp.repository;
+
+import com.bankapp.entity.LedgerEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LedgerRepository extends JpaRepository<LedgerEntry, UUID> {
+}
